841d63516cb253733b1b6bdbcb3fe3dcc247ed6e
Ashok Jaiswal <Ashok Jaiswal>
2016-05-31 19:46:23 -0700

    initial commit

---
+++ src/main/java/com/example/springtutorial/SpringAngularJsMongoDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularJsMongoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularJsMongoDemoApplication.class, args);
+	}
+}
